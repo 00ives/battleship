819a790e9ee04c4227c8c4e91c26ca259a2442f6
00ives <ivor.lewis@gmail.com>
2022-05-03 22:44:27 -0600

    p1 build player tables

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// function startGame() {
+let player1 = []
+let player2 = []
+player1 = buildGame(player1);
+player2 = buildGame(player2);
+// let player2 = buildg;
+
+printTable(player1 , 1)
+printTable(player2 , 2)
+// }
+
+
+function buildGame(player) {
+    player = [];
+    player.push('  A B C D E F G H');
+    for (let i = 1; i < 9; i++) {
+
+        player.push(i + ' - - - - - - - -');
+    }
+    return player;
+}
+
+function printTable(playerTable, playerNumber) {
+
+    if (playerNumber > 0) {
+        console.log("Player" + playerNumber + "'s Board");
+    }
+    for (let i = 0; i < player1.length; i++) {
+        console.log(player1[i]);
+
+    }
+}
+
